6a87ce986e9e71bc13bf9b592b6b6c737103f5fe
Matt Bauman <mbauman@juliahub.com>
2025-12-05 15:36:57 -0500

    fixup logic in artifact search

--- scripts/update_searchable_packages.jl
+++ scripts/update_searchable_packages.jl
@@ -8,7 +8,7 @@ function main()
     isfile(output_file) || touch(output_file)
     toml = TOML.parsefile(output_file)
     registry = Pkg.Registry.reachable_registries()[1]
-    for (_, pkg_info) in registry.pkgs
+    for (_, pkg_info) in Iterators.take(registry.pkgs, 10)
         pkg_dict = get!(toml, pkg_info.name, Dict{String,Any}())
         if !haskey(pkg_dict, "artifacts")
             # It's expensive to check for artifacts. Ideally this would also re-check
@@ -24,19 +24,18 @@ end
 
 function any_version_has_artifacts(pkg_info)
     Pkg.Registry.init_package_info!(pkg_info)
+    tmp = mktempdir()
     try
-        mktempdir() do tmp
-            repo_url = pkg_info.info.repo
-            subdir = pkg_info.info.subdir
-            run(pipeline(`git clone --bare $repo_url $tmp`, stdout=Base.devnull, stderr=Base.devnull))
-            for (_, verinfo) in pkg_info.info.version_info
-                cmd = `git -C $tmp ls-tree -r --name-only $(verinfo.git_tree_sha1)`
-                if !isnothing(subdir)
-                    cmd = `$cmd $subdir`
-                end
-                if success(pipeline(cmd, `grep -qE '(?:^|/)(?:Julia)?Artifacts\.toml$'`))
-                    return true
-                end
+        repo_url = pkg_info.info.repo
+        subdir = pkg_info.info.subdir
+        run(pipeline(`git clone --bare $repo_url $tmp`, stdout=Base.devnull, stderr=Base.devnull))
+        for (_, verinfo) in pkg_info.info.version_info
+            cmd = `git -C $tmp ls-tree -r --name-only $(verinfo.git_tree_sha1)`
+            if !isnothing(subdir)
+                cmd = `$cmd $subdir`
+            end
+            if success(pipeline(cmd, `grep -qE '(?:^|/)(?:Julia)?Artifacts\.toml$'`))
+                return true
             end
         end
     catch ex1
@@ -46,18 +45,18 @@ function any_version_has_artifacts(pkg_info)
             server = Pkg.Registry.pkg_server_registry_info()[1]
             for (_, verinfo) in pkg_info.info.version_info
                 url = "$server/package/$(pkg_info.uuid)/$(verinfo.git_tree_sha1)"
-                mktempdir() do d
-                    tarball = joinpath(d, "pkg.tar.gz")
-                    download(url, tarball)
-                    files = Tar.list(GzipDecompressorStream(open(tarball)))
-                    if any(x->occursin(r"(?:^|/)(?:Julia)?Artifacts\.toml$",x.path), files)
-                        return true
-                    end
+                tarball = joinpath(tmp, string(verinfo.git_tree_sha1, ".tar.gz"))
+                download(url, tarball)
+                files = Tar.list(GzipDecompressorStream(open(tarball)))
+                if any(x->occursin(r"(?:^|/)(?:Julia)?Artifacts\.toml$",x.path), files)
+                    return true
                 end
             end
         catch ex2
             @warn "Failed to check artifacts for package $(pkg_info.name)" ex1 ex2
         end
+    finally
+        rm(tmp; force=true, recursive=true)
     end
     return false
 end
